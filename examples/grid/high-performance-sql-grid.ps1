bc3fc63db6d0975b89fb7f4230bf54f554f1248c
Adam Driscoll <adamdriscoll@users.noreply.github.com>
2018-04-23 13:49:36 -0500

    Update high-performance-sql-grid.ps1

--- examples/grid/high-performance-sql-grid.ps1
+++ examples/grid/high-performance-sql-grid.ps1
@@ -8,20 +8,23 @@ $Dashboard = New-UDDashboard -Title "Grids - Simple" -Content {
     New-UDGrid -Title "Animals" -Headers @("Animal", "Order") -Properties @("Animal", "Order") -Endpoint {
         #Overrides filtering, sorting and paging. This ensures that SQL does the actions. 
         #All UDGrid endpioints have the following variables: 
-        # - $sortDescending - Whether or not the sort is descending
+        # - $sortAscending - Whether or not the sort is ascending
         # - $sortColumn - The name of the column to sort on 
         # - $filterText - The text to filter the data by
         # - $skip - The number of items to skip (used in paging)
         # - $take - The number of items to return (used in paging)
 
-        $sortOrder = "ASC"
-        if ($sortDescending ) {
-            $sortOrder = "DESC"
+        $sortOrder = "DESC"
+        if ($sortAscending ) {
+            $sortOrder = "ASC"
         }
+
+        $TotalItems = (Invoke-SqlCmd -Query "SELECT COUNT(*) as Total FROM animals WHERE Animal LIKE '%$filterText%' ").Total
+
         $Result = Invoke-SqlCmd -Query "SELECT Animal, Order FROM animals WHERE Animal LIKE '%$filterText%' ORDER BY $sortColumn $sortOrder OFFSET ($skip) ROWS FETCH NEXT ($take) ROWS ONLY " 
 
-        $Result | Out-UDGridData -NoAutoFilter -NoAutoPage -NoAutoSort
-    }
+        $Result | Out-UDGridData -TotalItems $TotalItems
+    } -ServerSideProcessing
 }
 
 Start-UDDashboard -Dashboard $Dashboard -Port 8080  
