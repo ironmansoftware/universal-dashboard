079dcf06ea752356bf47ba5498cdc38d0b964880
Adam Driscoll <adam.r.driscoll@gmail.com>
2018-07-09 00:11:06 -0500

    Use fullname.

--- src/UniversalDashboard.UITest/shebang.tests.ps1
+++ src/UniversalDashboard.UITest/shebang.tests.ps1
@@ -58,6 +58,6 @@ Stop-Service -Name UniversalDashboard -ErrorAction SilentlyContinue
 if ($null -ne $env:APPVEYOR_JOB_ID) {
     Get-ChildItem $OutputPath | ForEach-Object {
         $wc = New-Object 'System.Net.WebClient'
-        $wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit3/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $_))
+        $wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit3/$($env:APPVEYOR_JOB_ID)", ($_.FullName))
     }
 }
