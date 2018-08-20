0834e7ea8297b2b7b238377c6bc0abcff2df8c25
Adam Driscoll <adam.r.driscoll@gmail.com>
2018-08-20 14:37:22 -0600

    Fix tree view click handler.

--- src/UniversalDashboard/Cmdlets/NewTreeViewCommand.cs
+++ src/UniversalDashboard/Cmdlets/NewTreeViewCommand.cs
@@ -9,43 +9,55 @@ using System.Drawing;
 
 namespace UniversalDashboard.Cmdlets
 {
-	[Cmdlet(VerbsCommon.New, "UDTreeView")]
+    [Cmdlet(VerbsCommon.New, "UDTreeView")]
     public class NewTreeViewCommand : CallbackCmdlet
     {
-		private readonly Logger Log = LogManager.GetLogger(nameof(NewTreeNodeCommand));
+        private readonly Logger Log = LogManager.GetLogger(nameof(NewTreeNodeCommand));
 
         [Parameter(Mandatory = true)]
-		public TreeNode Node  { get; set; }
-
-		[Parameter]
-		public ScriptBlock OnNodeClicked { get; set; }
+        public TreeNode Node { get; set; }
 
         [Parameter]
-        public object[] OnNodeClickedArgumentList { get; set; }
+        public object OnNodeClicked { get; set; }
 
         [Parameter]
-		public DashboardColor BackgroundColor { get; set; } = new DashboardColor(Color.White);
+        public DashboardColor BackgroundColor { get; set; } = new DashboardColor(Color.White);
 
-		[Parameter]	
-		public DashboardColor FontColor { get; set; } = new DashboardColor(Color.Black);
+        [Parameter]
+        public DashboardColor FontColor { get; set; } = new DashboardColor(Color.Black);
 
-		[Parameter]	
-		public DashboardColor ActiveBackgroundColor { get; set; } = new DashboardColor(0xDFE8E4);
+        [Parameter]
+        public DashboardColor ActiveBackgroundColor { get; set; } = new DashboardColor(0xDFE8E4);
 
-		[Parameter]	
-		public DashboardColor ToggleColor { get; set; } = new DashboardColor(Color.Black);
+        [Parameter]
+        public DashboardColor ToggleColor { get; set; } = new DashboardColor(Color.Black);
 
-		protected override void EndProcessing()
-		{
-			WriteObject(new TreeView {
-				Id = Id,
-				Callback = GenerateCallback(Id, OnNodeClicked, OnNodeClickedArgumentList),
+        protected override void EndProcessing()
+        {
+            Endpoint callback = null;
+
+            var scriptBlock = OnNodeClicked as ScriptBlock;
+            if (scriptBlock != null)
+            {
+                callback = GenerateCallback(Id, scriptBlock, null);
+            }
+            else
+            {
+                var psobject = OnNodeClicked as PSObject;
+                callback = psobject?.BaseObject as Endpoint;
+
+            }
+
+            WriteObject(new TreeView
+            {
+                Id = Id,
+                Callback = callback,
                 Node = Node,
-				ToggleColor = ToggleColor?.HtmlColor,
-				BackgroundColor = BackgroundColor?.HtmlColor,
-				FontColor = FontColor?.HtmlColor,
-				ActiveBackgroundColor = ActiveBackgroundColor?.HtmlColor
+                ToggleColor = ToggleColor?.HtmlColor,
+                BackgroundColor = BackgroundColor?.HtmlColor,
+                FontColor = FontColor?.HtmlColor,
+                ActiveBackgroundColor = ActiveBackgroundColor?.HtmlColor
             });
-		}
-	}
+        }
+    }
 }
