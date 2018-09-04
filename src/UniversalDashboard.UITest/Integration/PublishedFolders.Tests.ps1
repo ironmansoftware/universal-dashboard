d7a328cce53613ed9154d8a6b569f6a31490c06d
Adam Driscoll <adam.r.driscoll@gmail.com>
2018-09-04 08:58:44 -0600

    Adding more tests.

--- src/UniversalDashboard.UITest/Integration/PublishedFolders.Tests.ps1
+++ src/UniversalDashboard.UITest/Integration/PublishedFolders.Tests.ps1
@@ -15,12 +15,28 @@ Describe "PublishedFolders" {
 
         "Test" | Out-File $TempFile -Force -Encoding ascii
 
+        $TempFile2 = Join-Path $TempPath "myFile.log"
+
+        "Test2" | Out-File $TempFile2 -Force -Encoding ascii
+
+        $TempFile3 = Join-Path $TempPath "myFile.ps1"
+
+        "Test3" | Out-File $TempFile3 -Force -Encoding ascii
+
         $Server = Start-UDRestApi -Port 10001 -PublishedFolder @(
             Publish-UDFolder -Path $TempPath -RequestPath "/temp"
         )
 
         It "should publish folder" {
             (Invoke-WebRequest http://localhost:10001/temp/myFile.txt).Content | Should be "Test`r`n"
+        }
+
+        It "should publish folder (log)" {
+            (Invoke-WebRequest http://localhost:10001/temp/myFile.log).Content | Should be "Test2`r`n"
+        }
+
+        It "should publish folder (ps1)" {
+            (Invoke-WebRequest http://localhost:10001/temp/myFile.ps1).Content | Should be "Test3`r`n"
         }
 
         Stop-UDRestApi $Server
@@ -33,6 +49,15 @@ Describe "PublishedFolders" {
 
         "Test" | Out-File $TempFile -Force -Encoding ascii
 
+        $TempFile2 = Join-Path $TempPath "myFile.log"
+
+        "Test" | Out-File $TempFile2 -Force -Encoding ascii
+
+        $TempFile3 = Join-Path $TempPath "myFile.ps1"
+
+        "Test" | Out-File $TempFile3 -Force -Encoding ascii
+
+
         $Server = Start-UDDashboard -Port 10001 -PublishedFolder @(
             Publish-UDFolder -Path $TempPath -RequestPath "/temp"
         ) -Dashboard (
@@ -41,6 +66,14 @@ Describe "PublishedFolders" {
 
         It "should publish folder" {
             (Invoke-WebRequest http://localhost:10001/temp/myFile.txt).Content | Should be "Test`r`n"
+        }
+
+        It "should publish folder (log)" {
+            (Invoke-WebRequest http://localhost:10001/temp/myFile.log).Content | Should be "Test2`r`n"
+        }
+
+        It "should publish folder (ps1)" {
+            (Invoke-WebRequest http://localhost:10001/temp/myFile.ps1).Content | Should be "Test3`r`n"
         }
 
         Stop-UDDashboard $Server
