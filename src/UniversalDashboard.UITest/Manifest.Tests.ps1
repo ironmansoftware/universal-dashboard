e912dd9f9cfdf3d54655b1d59341192249738ebc
Adam Driscoll <adam.r.driscoll@gmail.com>
2018-09-04 15:06:47 -0600

    Fix manifest test.

--- src/UniversalDashboard.UITest/Manifest.Tests.ps1
+++ src/UniversalDashboard.UITest/Manifest.Tests.ps1
@@ -11,7 +11,7 @@ Get-UDDashboard | Stop-UDDashboard
 Describe "Manifest" {
 
     It "should have correct version" {
-        (Get-Module 'UniversalDashboard.Community').Version | Should be "2.0.0"
+        (Get-Module 'UniversalDashboard.Community').Version | Should be "2.0.1"
     }
 
     It "should have correct exported commands" {
