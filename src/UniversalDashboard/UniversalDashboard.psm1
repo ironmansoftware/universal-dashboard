91eddfae082f2a6885222a84a72826465b2d3d46
Adam Driscoll <adamdriscoll@users.noreply.github.com>
2018-10-27 10:21:20 -0600

    Revert "Fix check for .NET Framework."

--- src/UniversalDashboard/UniversalDashboard.psm1
+++ src/UniversalDashboard/UniversalDashboard.psm1
@@ -1,14 +1,12 @@
 if ($PSEdition -eq "Core") {
 	Import-Module -Name "$PSScriptRoot/netstandard2.0/UniversalDashboard.dll" | Out-Null
 } else {
-	$MinDotNetVersion = 461814
-	[int]$DotNetRelease = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\').Release
-
-	if ($DotNetRelease -le $MinDotNetVersion) {
-		throw "Universal Dashboard requires .NET Framework version 4.7.2 or later when running within Windows PowerShell. https://www.microsoft.com/net/download/dotnet-framework-runtime"
+	$DotNetVersion = [Version](Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\').Version
+	if ($DotNetVersion -lt [Version]::new(4, 7)) {
+		throw "Universal Dashboard requires .NET Framework version 4.7 or later when running within Windows PowerShell"
 	}
 
-	Import-Module -Name "$PSScriptRoot/net472/UniversalDashboard.dll" | Out-Null
+	Import-Module -Name "$PSScriptRoot/net471/UniversalDashboard.dll" | Out-Null
 }
 
 Import-Module (Join-Path $PSScriptRoot "UniversalDashboardServer.psm1")
--- src/build.ps1
+++ src/build.ps1
@@ -24,8 +24,8 @@ if ($powerShellGet.Version -lt ([Version]'1.6.0')) {
 & dotnet publish -c $Configuration "$PSScriptRoot\UniversalDashboard\UniversalDashboard.csproj" -f netstandard2.0
 & dotnet publish -c $Configuration "$PSScriptRoot\UniversalDashboard.Server\UniversalDashboard.Server.csproj" -f netstandard2.0 
 
-& dotnet publish -c $Configuration "$PSScriptRoot\UniversalDashboard\UniversalDashboard.csproj" -f net472
-& dotnet publish -c $Configuration "$PSScriptRoot\UniversalDashboard.Server\UniversalDashboard.Server.csproj" -f net472
+& dotnet publish -c $Configuration "$PSScriptRoot\UniversalDashboard\UniversalDashboard.csproj" -f net471
+& dotnet publish -c $Configuration "$PSScriptRoot\UniversalDashboard.Server\UniversalDashboard.Server.csproj" -f net471
 
 Push-Location "$PSScriptRoot\client"
 
@@ -41,19 +41,19 @@ if ((Test-Path $outputDirectory)) {
 
 New-Item -ItemType Directory $outputDirectory
 
-$net472 = Join-Path $outputDirectory "net472"
+$net471 = Join-Path $outputDirectory "net471"
 $netstandard20 = Join-Path $outputDirectory "netstandard2.0"
 $help = Join-Path $outputDirectory "en-US"
 $client = Join-Path $outputDirectory "client"
 $poshud = Join-Path $outputDirectory "poshud"
 
-New-Item -ItemType Directory $net472
+New-Item -ItemType Directory $net471
 New-Item -ItemType Directory $netstandard20
 New-Item -ItemType Directory $help
 New-Item -ItemType Directory $client
 
 Copy-Item "$PSScriptRoot\UniversalDashboard\bin\$Configuration\netstandard2.0\publish\*" $netstandard20 -Recurse
-Copy-Item "$PSScriptRoot\UniversalDashboard\bin\$Configuration\net472\publish\*" $net472 -Recurse
+Copy-Item "$PSScriptRoot\UniversalDashboard\bin\$Configuration\net471\publish\*" $net471 -Recurse
 
 Copy-Item "$PSScriptRoot\client\src\public\*" $client -Recurse
 
@@ -61,16 +61,16 @@ Copy-Item "$PSScriptRoot\UniversalDashboard.Server\bin\$Configuration\netstandar
 Copy-Item "$PSScriptRoot\UniversalDashboard.Server\bin\$Configuration\netstandard2.0\publish\UniversalDashboard.Server.deps.json" $netstandard20
 Copy-Item "$PSScriptRoot\UniversalDashboard.Server\bin\$Configuration\netstandard2.0\publish\DasMulli.Win32.ServiceUtils.dll" $netstandard20
 
-Copy-Item "$PSScriptRoot\UniversalDashboard.Server\bin\$Configuration\net472\publish\UniversalDashboard.Server.exe" $net472
-Copy-Item "$PSScriptRoot\UniversalDashboard.Server\bin\$Configuration\net472\publish\DasMulli.Win32.ServiceUtils.dll" $net472
+Copy-Item "$PSScriptRoot\UniversalDashboard.Server\bin\$Configuration\net471\publish\UniversalDashboard.Server.exe" $net471
+Copy-Item "$PSScriptRoot\UniversalDashboard.Server\bin\$Configuration\net471\publish\DasMulli.Win32.ServiceUtils.dll" $net471
 
 Copy-Item "$PSScriptRoot\web.config" $outputDirectory
 Copy-Item "$PSScriptRoot\UniversalDashboard\UniversalDashboard.psm1" $outputDirectory
 Copy-Item "$PSScriptRoot\UniversalDashboard\UniversalDashboardServer.psm1" $outputDirectory
-Copy-Item "$PSScriptRoot\UniversalDashboard\bin\$Configuration\net472\UniversalDashboard.Controls.psm1" $outputDirectory
+Copy-Item "$PSScriptRoot\UniversalDashboard\bin\$Configuration\net471\UniversalDashboard.Controls.psm1" $outputDirectory
 Copy-Item "$PSScriptRoot\poshud" $poshud -Recurse -Container
 
-. "$PSScriptRoot\CorFlags.exe" /32BITREQ-  "$outputDirectory\net472\UniversalDashboard.Server.exe" 
+. "$PSScriptRoot\CorFlags.exe" /32BITREQ-  "$outputDirectory\net471\UniversalDashboard.Server.exe" 
 
 . (Join-Path $PSScriptRoot 'UniversalDashboard\New-UDModuleManifest.ps1') -outputDirectory $outputDirectory
 
