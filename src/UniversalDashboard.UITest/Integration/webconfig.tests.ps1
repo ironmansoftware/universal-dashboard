fb3c17689fbf783f81880d505fc4d6316bb03f0a
Adam Driscoll <adam.r.driscoll@gmail.com>
2018-10-27 10:37:05 -0600

    NET Check

---
+++ src/UniversalDashboard.UITest/Integration/webconfig.tests.ps1
@@ -0,0 +1,6 @@
+Describe "web.config" {
+    It "has correct framework version" {
+        $webconfig = Get-Content (Join-Path $PSScriptRoot "..\..\web.config") -Raw
+        $webconfig.Contains("net472") | Should be $true
+    }
+} 
