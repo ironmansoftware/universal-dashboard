8914c3a7e8690a75b26a4d98c199e1b0f6eb1dc8
Alon Gvili <poshil@outlook.co.il>
2018-11-12 00:33:47 +0200

    chnage terminal color

--- src/client/src/app/ud-terminal.jsx
+++ src/client/src/app/ud-terminal.jsx
@@ -5,14 +5,22 @@ import {fetchPostRaw} from './services/fetch-service.jsx';
 export default class UDTerminal extends React.Component {
     render() {
         return (
-            <Terminal commandPassThrough={(cmd, print) => {
+            <Terminal 
+                commandPassThrough={(cmd, print) => {
 
-                const text = cmd.join(' ');
+                    const text = cmd.join(' ');
 
-                fetchPostRaw(`/api/internal/component/terminal`, text, res => {
-                    print(res);
-                 });
-              }} watchConsoleLogging={true} promptSymbol={"UD >"} hideTopBar={false} allowTabs={true} startState={'maximised'}/>
+                    fetchPostRaw(`/api/internal/component/terminal`, text, res => {
+                        print(res);
+                    });
+                }} 
+                watchConsoleLogging={true} 
+                promptSymbol={"UD >"} 
+                hideTopBar={false} 
+                allowTabs={true} 
+                startState={'maximised'} 
+                backgroundColor='#07387e'
+                color='#fff'/>
         )
     }
 }
