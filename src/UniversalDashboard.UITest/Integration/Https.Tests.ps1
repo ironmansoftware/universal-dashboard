dd692a2281e89c2987052f14099f1e1da5cfd0fa
Alon Gvili <poshil@outlook.co.il>
2018-11-14 00:20:45 +0200

    work around https cert dashboard test

--- src/UniversalDashboard.UITest/Integration/Https.Tests.ps1
+++ src/UniversalDashboard.UITest/Integration/Https.Tests.ps1
@@ -5,6 +5,18 @@ $ModulePath = Get-ModulePath -Release:$Release
 
 Import-Module $ModulePath -Force
 
+add-type @”
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+public class TrustAllCertsPolicy : ICertificatePolicy {
+public bool CheckValidationResult(
+ServicePoint srvPoint, X509Certificate certificate,
+WebRequest request, int certificateProblem) {
+return true;
+}
+}
+“@
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 Describe "Https" {
