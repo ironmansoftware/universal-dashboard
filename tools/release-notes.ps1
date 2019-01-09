e4c4ab9060e928cae4ea43294a91796ea528f87e
Adam Driscoll <adam.r.driscoll@gmail.com>
2019-01-09 11:47:36 -0700

    Add release note script.

---
+++ tools/release-notes.ps1
@@ -0,0 +1,36 @@
+param($MilestoneName = "2.2.1")
+
+[System.Net.ServicePointManager]::SecurityProtocol = @("Tls12","Tls11","Tls","Ssl3")
+
+$Milestone = (invoke-restmethod "https://api.github.com/repos/ironmansoftware/universal-dashboard/milestones?state=open") | Where-Object title -eq $MilestoneName
+$Issues = invoke-restmethod "https://api.github.com/repos/ironmansoftware/universal-dashboard/issues?milestone=$($Milestone.number)&state=closed"
+
+$Bugs = $Issues | Where-Object { $_.Labels.Name -eq "Bug" }
+$Enhancements = $Issues | Where-Object { $_.Labels.Name -eq "Enhancement" }
+
+$Markdown = ""
+
+$Markdown += "# Enhancements " 
+$Markdown += [Environment]::NewLine
+
+foreach($Enhancement in $Enhancements) {
+    $Markdown += [Environment]::NewLine
+    $Markdown +="## ($($Enhancement.title))[$($Enhancement.html_url)]"
+}
+
+$Markdown += [Environment]::NewLine
+$Markdown += [Environment]::NewLine
+
+$Markdown += "# Bug Fixes" 
+
+foreach($Bug in $Bugs) {
+    $Markdown += [Environment]::NewLine
+    $Markdown +="## ($($Bug.title))[$($Bug.html_url)] - Reported by @($($Bug.user.login))[$($Bug.user.html_url)]"
+}
+
+$Markdown
+
+
+
+
+
