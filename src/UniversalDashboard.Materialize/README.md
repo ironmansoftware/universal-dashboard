d0d309857c57f80061fbd2b2f8e39e666e1aa25c
Adam Driscoll <adam.r.driscoll@gmail.com>
2019-01-28 05:08:18 -0700

    Add some more tests.

--- src/UniversalDashboard.Materialize/README.md
+++
@@ -1,1 +0,0 @@
-UD-Charts
---
+++ src/UniversalDashboard.UITest/Cmdlet/New-UDTab.Tests.ps1
@@ -0,0 +1,21 @@
+param([Switch]$Release)
+
+Import-Module "$PSScriptRoot\..\TestFramework.psm1" -Force
+$ModulePath = Get-ModulePath -Release:$Release
+Import-Module $ModulePath -Force
+Describe "New-UDTab" {
+    It "should support text parameter" {
+        $Image = New-UDTab -Text 'hi' -Content {}
+        $Image.Text | should be "hi"
+    }
+
+    It "should be plugin" {
+        $Image = New-UDTab -Text 'hi' -Content {}
+        $Image.isPlugin | should be $true
+    }
+
+    It "should have type of tab" {
+        $Image = New-UDTab -Text 'hi' -Content {}
+        $Image.type | should be 'tab'
+    }
+}
