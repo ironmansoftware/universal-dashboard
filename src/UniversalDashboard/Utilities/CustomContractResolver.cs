1fc561b8ef2ed704f82ecb148e24add2600541a2
Adam Driscoll <adamdriscoll@users.noreply.github.com>
2019-01-31 11:48:00 -0700

    Fix some issues with serialization. (#604)

---
+++ src/UniversalDashboard/Utilities/CustomContractResolver.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections;
+using System.Management.Automation;
+using UniversalDashboard.Models;
+
+namespace UniversalDashboard.Utilities
+{
+    public class CustomContractResolver : DefaultContractResolver
+    {
+        protected override JsonConverter ResolveContractConverter(Type objectType)
+        {
+            if (objectType.IsAssignableFrom(typeof(GenericComponent)))
+            {
+                return new GenericComponentJsonConvert();
+            }
+
+            if (typeof(PSObject).IsAssignableFrom(objectType) || typeof(Hashtable).IsAssignableFrom(objectType))
+            {
+                return new PSObjectJsonConvert();
+            }
+
+            return base.ResolveContractConverter(objectType);
+        }
+
+        //protected override JsonObjectContract CreateObjectContract(Type objectType)
+        //{
+        //    JsonObjectContract contract = base.CreateObjectContract(objectType);
+        //    if (objectType.IsAssignableFrom(typeof(GenericComponent)))
+        //    {
+        //        contract.Converter = new GenericComponentJsonConvert();
+        //    }
+
+        //    if (typeof(PSObject).IsAssignableFrom(objectType) || typeof(Hashtable).IsAssignableFrom(objectType))
+        //    {
+        //        contract.Converter = new PSObjectJsonConvert();
+        //    }
+
+        //    return contract;
+        //}
+
+        protected override string ResolvePropertyName(string propertyName)
+        {
+            propertyName = base.ResolvePropertyName(propertyName);
+            return char.ToLowerInvariant(propertyName[0]) + propertyName.Substring(1);
+        }
+    }
+}
---
+++ src/UniversalDashboard/Utilities/PSObjectJsonConvert.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Management.Automation;
+
+namespace UniversalDashboard.Utilities
+{
+    public class PSObjectJsonConvert : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(PSObject).IsAssignableFrom(objectType) || typeof(Hashtable).IsAssignableFrom(objectType);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            return null;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var dictionary = value.ToDictionary() as Dictionary<string, object>;
+
+            writer.WriteStartObject();
+           
+            foreach (var property in dictionary)
+            {
+                writer.WritePropertyName(property.Key);
+                serializer.Serialize(writer, property.Value);
+            }
+
+            writer.WriteEndObject();
+        }
+    }
+}
