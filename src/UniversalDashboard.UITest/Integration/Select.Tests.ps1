fb964aee72779009c9c5326dd139a31f6560d069
Adam Driscoll <adamdriscoll@users.noreply.github.com>
2019-02-14 14:00:49 -0700

    Fixes #628 (#635)
    * Fixes #628
    
    * Make test less flaky.

--- src/UniversalDashboard.UITest/Integration/Select.Tests.ps1
+++ src/UniversalDashboard.UITest/Integration/Select.Tests.ps1
@@ -11,18 +11,30 @@ Get-UDDashboard | Stop-UDDashboard
 Describe "Select" {
     Context "onSelect" {
         $dashboard = New-UDDashboard -Title "Test" -Content {
+
+            New-UDButton -Text "Button" -Id 'btn' -OnClick {
+                $Value = (Get-UDElement -Id 'test').Attributes['value'] 
+                Add-UDElement -ParentId 'output' -Content {
+                    New-UDElement -Tag 'div' -Id 'innerOutput2' -Content { $value }
+                }
+            }
+
             New-UDSelect -Label "Test" -Id 'test' -Option {
                 New-UDSelectOption -Nam "Test 1" -Value "1"
                 New-UDSelectOption -Nam "Test 2" -Value "2"
                 New-UDSelectOption -Nam "Test 3" -Value "3"
             } -OnChange {
-                Set-UDElement -Id "output" -Content { $EventData }
+                Add-UDElement -ParentId 'output' -Content {
+                    New-UDElement -Tag 'div' -Id 'innerOutput' -Content { $EventData }
+                }
             }
 
             New-UDElement -Id "output" -Tag "div" -Content { }
+
+
         }
 
-        $Server = Start-UDDashboard -Port 10001 -Dashboard $dashboard 
+        $Server = Start-UDDashboard -Port 10001 -Dashboard $dashboard -Design 
         $Driver = Start-SeFirefox
         Enter-SeUrl -Driver $Driver -Url "http://localhost:$BrowserPort"
 
@@ -33,7 +45,20 @@ Describe "Select" {
             $Element = Find-SeElement -XPath "//ul/li" -Element $Element | Select-Object -Skip 0 -First 1
             Invoke-SeClick -Element $Element
 
-            (Find-SeElement -Driver $Driver -Id 'output').Text | should be "1"
+            (Find-SeElement -Driver $Driver -Id 'innerOutput').Text | should be "1"
+        }
+
+        It "should select item and get it with Get-UDElement" {
+            $Element = Find-SeElement -ClassName "select-wrapper" -Driver $Driver | Select-Object -First 1
+            Invoke-SeClick -Element $Element
+            
+            $Element = Find-SeElement -XPath "//ul/li" -Element $Element | Select-Object -Skip 0 -First 1
+            Invoke-SeClick -Element $Element
+
+            $Button = Find-SeElement -Driver $Driver -Id 'btn'
+            Invoke-SeClick -Element $Button
+
+            (Find-SeElement -Driver $Driver -Id 'innerOutput2').Text | should be "1"
         }
 
        Stop-SeDriver $Driver
