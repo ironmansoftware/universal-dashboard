30312446eadb12a419f5fab66135edc45c68e43b
Adam Driscoll <adam.r.driscoll@gmail.com>
2019-02-18 17:43:08 -0600

    Fixes #645

--- src/UniversalDashboard.UITest/Manifest.Tests.ps1
+++ src/UniversalDashboard.UITest/Manifest.Tests.ps1
@@ -11,7 +11,7 @@ Get-UDDashboard | Stop-UDDashboard
 Describe "Manifest" {
 
     It "should have correct version" {
-        (Get-Module 'UniversalDashboard.Community').Version | Should be "2.3.0"
+        (Get-Module 'UniversalDashboard.Community').Version | Should be "2.3.2"
     }
 
     It "should have correct exported commands" {
