004d8824b8f791fbd6c1e26f444d049113ba997f
Adam Driscoll <adamdriscoll@users.noreply.github.com>
2019-03-27 14:15:37 -0600

    Implements #561 (#709)
    * Bump version
    
    * Not found page.

---
+++ src/client/src/app/not-found.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class NotFound extends React.Component {
+    render() {
+        return (
+            <h1 className="center-align" id='notfound'><i className='fa fa-question-circle grey-text'/> Page Not Found</h1>
+        )
+    }
+}
