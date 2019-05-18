55af5f50b12fad5aa59d9ba0f4833bb4210be5c5
Adam Driscoll <adam.r.driscoll@gmail.com>
2019-05-17 20:27:53 -0600

    Fixes #258

--- src/UniversalDashboard.Materialize/Components/custom-cell.jsx
+++ src/UniversalDashboard.Materialize/Components/custom-cell.jsx
@@ -6,12 +6,13 @@ export default class CustomCell extends React.Component {
         const validComponents = ["link", "icon", "image", "element"]
         
         var y = this.props.value;
+        var moment2 = moment(y);
 
         if (y == null) {
             return <span></span>;
         }        
         if (y.DateTime) {
-            var moment2 = moment(y.value);
+            moment2 = moment(y.value);
             if (moment2.isValid) {
                 return <span>{moment2.format(this.props.dateTimeFormat)}</span>;
             }
@@ -19,8 +20,14 @@ export default class CustomCell extends React.Component {
                 return <span>{y.DateTime}</span>;
             }
         }
+        else if (moment2.isValid) {
+            
+            if (moment2.isValid) {
+                return <span>{moment2.format(this.props.dateTimeFormat)}</span>;
+            }
+        }
         else if (y.startsWith && y.startsWith("/Date(")){
-            var moment2 = moment(y.substr(6), 'x)/"');
+            moment2 = moment(y.substr(6), 'x)/"');
             if (moment2.isValid) {
                 return <span>{moment2.format(this.props.dateTimeFormat)}</span>;
             }
