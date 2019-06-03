f4bc29eedd79de20a7bcd2a8f39692976709ff26
Adam Driscoll <adam.r.driscoll@gmail.com>
2019-06-03 10:59:05 -0600

    Fixes #853

--- src/UniversalDashboard.UITest/TestFramework.psm1
+++ src/UniversalDashboard.UITest/TestFramework.psm1
@@ -1,5 +1,15 @@
 Import-Module "$PSScriptRoot\..\Selenium\Selenium.psm1" -Force 
 
+function Get-TestData {
+    $Data = $null
+    if ($Cache:StateCollection.TryTake([ref]$Data, 5000)) {
+        $Data 
+    } 
+    else {
+        throw "Retreiving data timed out (5000ms)."
+    }
+}
+
 function Get-ModulePath {
     param([Switch]$Release)
 
