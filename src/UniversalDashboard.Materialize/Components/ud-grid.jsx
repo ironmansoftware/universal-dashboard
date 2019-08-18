7bbd966d626b4299773e46720e5c2672881df213
psDevUK <adambacon1@hotmail.com>
2019-08-18 20:44:21 +0100

    Fixes #1004

--- src/UniversalDashboard.Materialize/Components/ud-grid.jsx
+++ src/UniversalDashboard.Materialize/Components/ud-grid.jsx
@@ -4,19 +4,19 @@ import Griddle, { RowDefinition, ColumnDefinition, plugins } from 'griddle-react
 import ErrorCard from './error-card.jsx';
 import UdLink from './ud-link.jsx';
 import CustomCell from './custom-cell.jsx';
-import {DebounceInput} from 'react-debounce-input';
+import { DebounceInput } from 'react-debounce-input';
 import { Dropdown, Button, Row, Col } from 'react-materialize';
 import UdIcon from './ud-icon.jsx';
-
+import { saveAs } from 'file-saver';
 function strMapToObj(strMap) {
     if (strMap == undefined) return null;
     if (strMap._tail != undefined && strMap._tail.array != undefined) {
         return strMap._tail.array.map(x => strMapToObj(x));
-    } 
+    }
     if (!strMap.__iterate) return strMap;
 
     let obj = Object.create({});
-    for (let [k,v] of strMap) {
+    for (let [k, v] of strMap) {
         // We don’t escape the key '__proto__'
         // which can cause problems on older engines
         obj[k] = strMapToObj(v);
@@ -41,23 +41,23 @@ export default class UdGrid extends React.Component {
         } else if (defaultSortColumn == null) {
             defaultSortColumn = ''
         }
-    
+
         this.state = {
-          data: [],
-          currentPage: 1,
-          pageSize: props.pageSize,
-          recordCount: 0,
-          hasError: false,
-          errorMessage: "",
-          sortColumn: defaultSortColumn,
-          sortAscending: !props.defaultSortDescending,
-          filterText: "",
-          properties: props.properties,
-          headers: props.headers,
-          loading: true,
-          firstLoad: true
+            data: [],
+            currentPage: 1,
+            pageSize: props.pageSize,
+            recordCount: 0,
+            hasError: false,
+            errorMessage: "",
+            sortColumn: defaultSortColumn,
+            sortAscending: !props.defaultSortDescending,
+            filterText: "",
+            properties: props.properties,
+            headers: props.headers,
+            loading: true,
+            firstLoad: true
         };
-      }
+    }
 
     onIncomingEvent(eventName, event) {
         if (event.type === "syncElement") {
@@ -67,17 +67,17 @@ export default class UdGrid extends React.Component {
 
     componentWillUnmount() {
         this.state.data.forEach(x => {
-            for(var propertyName in x) {
+            for (var propertyName in x) {
                 var property = x[propertyName];
                 if (property == null) continue;
                 if (property.type === "element") {
-                    for(var i = 0; i < property.events.length; i++) {
+                    for (var i = 0; i < property.events.length; i++) {
                         UniversalDashboard.publish('element-event', {
                             type: "unregisterEvent",
                             eventId: property.events[i].id
                         });
                     }
-    
+
                     if (property.hasCallback) {
                         UniversalDashboard.publish('element-event', {
                             type: "unregisterEvent",
@@ -104,8 +104,8 @@ export default class UdGrid extends React.Component {
             loading: true
         })
 
-        UniversalDashboard.get(`/api/internal/component/datatable/${this.props.id}?start=${skip}&length=${pageSize}&sortColumn=${sortColumn}&sortAscending=${sortAscending}&filterText=${filterText}`, function(json){
-            
+        UniversalDashboard.get(`/api/internal/component/datatable/${this.props.id}?start=${skip}&length=${pageSize}&sortColumn=${sortColumn}&sortAscending=${sortAscending}&filterText=${filterText}`, function (json) {
+
             this.setState({
                 loading: false,
                 firstLoad: false
@@ -117,7 +117,7 @@ export default class UdGrid extends React.Component {
                     errorMessage: json.error.message
                 })
             } else {
-               
+
                 if (!json.data.length) {
                     json.data = [json.data]
                 }
@@ -126,12 +126,12 @@ export default class UdGrid extends React.Component {
                     return
                 }
 
-                if (json.data.length == 1 && typeof json.data[0] === 'string')  {
+                if (json.data.length == 1 && typeof json.data[0] === 'string') {
                     return;
                 }
 
-                if (Object.prototype.toString.call( json.data ) === '[object Array]' && json.data.length === 1) {
-                    if (Object.prototype.toString.call( json.data[0] ) === '[object Array]' && json.data[0].length === 0)
+                if (Object.prototype.toString.call(json.data) === '[object Array]' && json.data.length === 1) {
+                    if (Object.prototype.toString.call(json.data[0]) === '[object Array]' && json.data[0].length === 0)
                         return;
                 }
 
@@ -148,11 +148,11 @@ export default class UdGrid extends React.Component {
         }.bind(this));
     }
 
-    updateTableState({data, currentPage, recordCount, sortColumn, sortAscending, filterText}) {
-        if (Object.prototype.toString.call( data ) !== '[object Array]' ) {
+    updateTableState({ data, currentPage, recordCount, sortColumn, sortAscending, filterText }) {
+        if (Object.prototype.toString.call(data) !== '[object Array]') {
             data = []
         }
-        this.setState({ data, currentPage, recordCount, hasError: false, sortColumn, sortAscending , filterText});
+        this.setState({ data, currentPage, recordCount, hasError: false, sortColumn, sortAscending, filterText });
     }
 
     onSort(sortProperties) {
@@ -196,7 +196,7 @@ export default class UdGrid extends React.Component {
 
         var csv = '';
         this.state.data.forEach(x => {
-            for(var propertyName in x) {
+            for (var propertyName in x) {
                 var property = x[propertyName];
                 if (property == null) continue;
 
@@ -205,16 +205,8 @@ export default class UdGrid extends React.Component {
             csv = csv.substr(0, csv.length - 1) + "\r\n";
         })
 
-        var element = document.createElement('a');
-        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(csv));
-        element.setAttribute('download', 'export.csv');
-      
-        element.style.display = 'none';
-        document.body.appendChild(element);
-      
-        element.click();
-      
-        document.body.removeChild(element);
+        var blob = new Blob([csv], { type: "text/csv;charset=utf-8" });
+        saveAs(blob, "export.csv", { autoBom: true })
     }
 
     onLocalSort(sortProperties) {
@@ -228,26 +220,26 @@ export default class UdGrid extends React.Component {
 
     render() {
         if (this.state.hasError) {
-            return [<ErrorCard message={this.state.errorMessage} title={this.props.title} id={this.props.id} key={this.props.id} />, <ReactInterval timeout={this.props.refreshInterval * 1000} enabled={this.props.autoRefresh} callback={this.loadData.bind(this)}/>]
+            return [<ErrorCard message={this.state.errorMessage} title={this.props.title} id={this.props.id} key={this.props.id} />, <ReactInterval timeout={this.props.refreshInterval * 1000} enabled={this.props.autoRefresh} callback={this.loadData.bind(this)} />]
         }
 
         var rowDefinition = null;
         if (this.props.headers) {
-            var columns = this.props.headers.map(function(x, i) {
-                return <ColumnDefinition key={i} id={this.props.properties[i]} title={x} customComponent={CustomColumn} dateTimeFormat={this.props.dateTimeFormat}/>
+            var columns = this.props.headers.map(function (x, i) {
+                return <ColumnDefinition key={i} id={this.props.properties[i]} title={x} customComponent={CustomColumn} dateTimeFormat={this.props.dateTimeFormat} />
             }.bind(this));
 
             rowDefinition = <RowDefinition>{columns}</RowDefinition>;
         } else if (this.state.data != null && this.state.data.length > 0) {
             var columns = [];
             var i = 0;
-            for(var key in this.state.data[0]) {
-                columns.push(<ColumnDefinition key={i.toString()} id={key} title={key} customComponent={CustomColumn} dateTimeFormat={this.props.dateTimeFormat}/>)
+            for (var key in this.state.data[0]) {
+                columns.push(<ColumnDefinition key={i.toString()} id={key} title={key} customComponent={CustomColumn} dateTimeFormat={this.props.dateTimeFormat} />)
                 i++;
             }
 
             rowDefinition = <RowDefinition>{columns}</RowDefinition>;
-        } 
+        }
 
         const styleConfig = {
             classNames: {
@@ -256,9 +248,9 @@ export default class UdGrid extends React.Component {
             }
         }
 
-        var actions = null 
+        var actions = null
         if (this.props.links) {
-            var links = this.props.links.map(function(x, i) {
+            var links = this.props.links.map(function (x, i) {
                 return <UdLink {...x} key={x.url} />
             });
             actions = <div className="card-action">
@@ -281,7 +273,7 @@ export default class UdGrid extends React.Component {
             if (this.props.noFilter) {
                 components = {
                     ...components,
-                    Filter: () => <span/>
+                    Filter: () => <span />
                 }
             }
             serverSort = this.onLocalSort.bind(this);
@@ -299,60 +291,60 @@ export default class UdGrid extends React.Component {
 
             components = {
                 ...components,
-                Filter: () => <span/>
+                Filter: () => <span />
             }
         }
 
         return (
-            <div className="card ud-grid" id={this.props.id} style={{background: this.props.backgroundColor, color: this.props.fontColor}} key={this.props.id}>
+            <div className="card ud-grid" id={this.props.id} style={{ background: this.props.backgroundColor, color: this.props.fontColor }} key={this.props.id}>
                 <div className="card-content">
                     <span className="card-title">{this.props.title}</span>
 
                     {serverFilterControl}
-                    { this.state.firstLoad ? 
-                    <div className="progress"><div className="indeterminate"></div></div> :
-                    rowDefinition ? 
-                    [<Griddle 
-                        data={this.state.data}
-                        plugins={gridPlugins}
-                        sortProperties={[{
-                            id: this.state.sortColumn,
-                            sortAscending: this.state.sortAscending
-                        }]}
-                        pageProperties={{
-                            currentPage: this.state.currentPage,
-                            pageSize: this.props.noPaging ? Number.MAX_SAFE_INTEGER : this.state.pageSize,
-                            recordCount: this.state.recordCount,
-                        }}
-                        events={{
-                            onSort: serverSort,
-                            onFilter: serverFilter,
-                            onNext: serverNext,
-                            onPrevious: serverPrev,
-                            onGetPage: serverGetPage,
-                        }}
-                        components={components}
-                        styleConfig={styleConfig}
-                    >
-                        {rowDefinition}
-                    </Griddle>,
-                    <GridToolbar 
-                        activePage={this.state.currentPage} 
-                        totalPages={Math.ceil(this.state.recordCount / this.state.pageSize)} 
-                        onPageChanged={this.onPageChanged.bind(this)}
-                        onExportData={this.onExportData.bind(this)}
-                        noExport={this.props.noExport}
-                        reload={this.reload.bind(this)}
-                        loading={this.state.loading}
-                        noPaging={this.props.noPaging}
-                        />]
-                     : <div>No results found</div>}
+                    {this.state.firstLoad ?
+                        <div className="progress"><div className="indeterminate"></div></div> :
+                        rowDefinition ?
+                            [<Griddle
+                                data={this.state.data}
+                                plugins={gridPlugins}
+                                sortProperties={[{
+                                    id: this.state.sortColumn,
+                                    sortAscending: this.state.sortAscending
+                                }]}
+                                pageProperties={{
+                                    currentPage: this.state.currentPage,
+                                    pageSize: this.props.noPaging ? Number.MAX_SAFE_INTEGER : this.state.pageSize,
+                                    recordCount: this.state.recordCount,
+                                }}
+                                events={{
+                                    onSort: serverSort,
+                                    onFilter: serverFilter,
+                                    onNext: serverNext,
+                                    onPrevious: serverPrev,
+                                    onGetPage: serverGetPage,
+                                }}
+                                components={components}
+                                styleConfig={styleConfig}
+                            >
+                                {rowDefinition}
+                            </Griddle>,
+                            <GridToolbar
+                                activePage={this.state.currentPage}
+                                totalPages={Math.ceil(this.state.recordCount / this.state.pageSize)}
+                                onPageChanged={this.onPageChanged.bind(this)}
+                                onExportData={this.onExportData.bind(this)}
+                                noExport={this.props.noExport}
+                                reload={this.reload.bind(this)}
+                                loading={this.state.loading}
+                                noPaging={this.props.noPaging}
+                            />]
+                            : <div>No results found</div>}
                 </div>
 
                 {actions}
-                <ReactInterval timeout={this.props.refreshInterval * 1000} enabled={this.props.autoRefresh} callback={this.reload.bind(this)}/>
+                <ReactInterval timeout={this.props.refreshInterval * 1000} enabled={this.props.autoRefresh} callback={this.reload.bind(this)} />
             </div>
-               );
+        );
     }
 }
 
@@ -372,7 +364,7 @@ class GridToolbar extends React.Component {
                 startPage = this.props.totalPages - 10;
             }
 
-            for(var i = startPage; i <= this.props.totalPages; i++) {
+            for (var i = startPage; i <= this.props.totalPages; i++) {
 
                 if (i <= 0) continue;
 
@@ -380,39 +372,39 @@ class GridToolbar extends React.Component {
                     break;
                 }
 
-                pages.push(<Page activePage={this.props.activePage} onPageChanged={this.props.onPageChanged} page={i} pointer/>);
+                pages.push(<Page activePage={this.props.activePage} onPageChanged={this.props.onPageChanged} page={i} pointer />);
             }
 
             if (this.props.totalPages >= 10 && this.props.activePage < (this.props.totalPages - 10)) {
                 pages.push(<Page activePage={this.props.activePage} page='...' />);
-                pages.push(<Page activePage={this.props.activePage} onPageChanged={this.props.onPageChanged} page={this.props.totalPages} pointer/>);
+                pages.push(<Page activePage={this.props.activePage} onPageChanged={this.props.onPageChanged} page={this.props.totalPages} pointer />);
             }
 
-            pagination = <ul className="pagination"  style={{display: 'inline-block'}} >
+            pagination = <ul className="pagination" style={{ display: 'inline-block' }} >
                 <li className={this.props.activePage === 1 ? "disabled" : ""} style={this.props.activePage > 1 ? cursor : {}}><a onClick={() => this.props.activePage > 1 && this.props.onPageChanged(this.props.activePage - 1)}><UdIcon icon="ChevronLeft" /></a></li>
-                    {pages}
-                <li className={this.props.activePage === this.props.totalPages ? "disabled" : ""}  style={this.props.activePage < this.props.totalPages ? cursor : {}}><a onClick={() => this.props.activePage < this.props.totalPages && this.props.onPageChanged(this.props.activePage + 1)}><UdIcon icon="ChevronRight" /></a></li>
+                {pages}
+                <li className={this.props.activePage === this.props.totalPages ? "disabled" : ""} style={this.props.activePage < this.props.totalPages ? cursor : {}}><a onClick={() => this.props.activePage < this.props.totalPages && this.props.onPageChanged(this.props.activePage + 1)}><UdIcon icon="ChevronRight" /></a></li>
             </ul>
         }
 
         return (
             <Row>
-                <Button 
-                    icon={<UdIcon icon="Sync" spin={this.props.loading} />} 
-                    style={{display: 'inline-block', float: 'right', marginTop: '15px', marginLeft: '10px'}} 
-                    onClick={this.props.reload} 
+                <Button
+                    icon={<UdIcon icon="Sync" spin={this.props.loading} />}
+                    style={{ display: 'inline-block', float: 'right', marginTop: '15px', marginLeft: '10px' }}
+                    onClick={this.props.reload}
                     flat
                     tooltip="Refresh"
-                    tooltipOptions={{position: 'bottom'}}
-                    />
-                <Button 
-                    icon={<UdIcon icon="Download" />} 
-                    style={{display: this.props.noExport ? 'none' : 'inline-block', float: 'right', marginTop: '15px'}} 
-                    onClick={this.props.onExportData} 
+                    tooltipOptions={{ position: 'bottom' }}
+                />
+                <Button
+                    icon={<UdIcon icon="Download" />}
+                    style={{ display: this.props.noExport ? 'none' : 'inline-block', float: 'right', marginTop: '15px' }}
+                    onClick={this.props.onExportData}
                     flat
                     tooltip="Export to CSV"
-                    tooltipOptions={{position: 'bottom'}}
-                    />
+                    tooltipOptions={{ position: 'bottom' }}
+                />
                 {pagination}
             </Row>
         )
