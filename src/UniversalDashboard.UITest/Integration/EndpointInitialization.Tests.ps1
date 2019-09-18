08bb1c88a19a3d07d6e5a167a9fcb8443c079c6d
Adam Driscoll <adam.r.driscoll@gmail.com>
2019-09-18 08:38:24 +0530

    Try fix test again.

--- src/UniversalDashboard.UITest/Integration/EndpointInitialization.Tests.ps1
+++ src/UniversalDashboard.UITest/Integration/EndpointInitialization.Tests.ps1
@@ -31,34 +31,34 @@ Describe "EndpointInitialization" {
 
         $dashboard = New-UDDashboard -Title "Test" -Content {} 
         $Server = Start-UDDashboard -Port 10001 -Dashboard $dashboard -Endpoint @(
-            New-UDEndpoint -Url '/module1' -Endpoint {
+            New-UDEndpoint -Url '/module1_a' -Endpoint {
                 Get-Number 
             }
-            New-UDEndpoint -Url "/variable" -Endpoint {
+            New-UDEndpoint -Url "/variable_a" -Endpoint {
                 $SomeVar123
             }
-            New-UDEndpoint -Url "/function" -Endpoint {
+            New-UDEndpoint -Url "/function_a" -Endpoint {
                 Get-Stuff
             }
-            New-UDEndpoint -Url "/module2" -Endpoint {
+            New-UDEndpoint -Url "/module2_a" -Endpoint {
                 Get-TheMeaningOfLife
             }
         )
 
         It "should load module from temp dir" {
-            Invoke-RestMethod "http://localhost:10001/api/module1" | Should be "10"
+            Invoke-RestMethod "http://localhost:10001/api/module1_a" | Should be "10"
         }
 
         It "should have variable defined" {
-            Invoke-RestMethod "http://localhost:10001/api/variable" | Should be "This is a value"
+            Invoke-RestMethod "http://localhost:10001/api/variable_a" | Should be "This is a value"
         }
 
         It "should have function defined" {
-            Invoke-RestMethod "http://localhost:10001/api/function" | Should be "999"
+            Invoke-RestMethod "http://localhost:10001/api/function_a" | Should be "999"
         }
 
         It "should load module from relative path" {
-            Invoke-RestMethod "http://localhost:10001/api/module2" | Should be "42"
+            Invoke-RestMethod "http://localhost:10001/api/module2_a" | Should be "42"
         }
 
         Remove-Item $tempModule -Force
@@ -92,41 +92,41 @@ Describe "EndpointInitialization" {
         $dashboard = New-UDDashboard -Title "Test" -Content {} -EndpointInitialization $Initialization
         
         $Server = Start-UDDashboard -Port 10001 -Dashboard $dashboard -Endpoint @(
-            New-UDEndpoint -Url '/module1' -Endpoint {
+            New-UDEndpoint -Url '/module1_b' -Endpoint {
                 Get-Number 
             }
-            New-UDEndpoint -Url "/variable" -Endpoint {
+            New-UDEndpoint -Url "/variable_b" -Endpoint {
                 $SomeVar123
             }
-            New-UDEndpoint -Url "/function" -Endpoint {
+            New-UDEndpoint -Url "/function_b" -Endpoint {
                 Get-Stuff
             }
-            New-UDEndpoint -Url "/module2" -Endpoint {
+            New-UDEndpoint -Url "/module2_b" -Endpoint {
                 Get-TheMeaningOfLife
             }
-            New-UDEndpoint -Url "/notfound" -Endpoint {
+            New-UDEndpoint -Url "/notfound_b" -Endpoint {
                 Get-NoStuff
             }
         )
 
         It "should load module from temp dir" {
-            Invoke-RestMethod "http://localhost:10001/api/module1" | Should be "10"
+            Invoke-RestMethod "http://localhost:10001/api/module1_b" | Should be "10"
         }
 
         It "should have variable defined" {
-            Invoke-RestMethod "http://localhost:10001/api/variable" | Should be "This is a value"
+            Invoke-RestMethod "http://localhost:10001/api/variable_b" | Should be "This is a value"
         }
 
         It "should have function defined" {
-            Invoke-RestMethod "http://localhost:10001/api/function" | Should be "999"
+            Invoke-RestMethod "http://localhost:10001/api/function_b" | Should be "999"
         }
 
         It "should load module from relative path" {
-            Invoke-RestMethod "http://localhost:10001/api/module2" | Should be "42"
+            Invoke-RestMethod "http://localhost:10001/api/module2_b" | Should be "42"
         }
 
         It "should not load function" {
-            Invoke-RestMethod "http://localhost:10001/api/notfound" | Should not be 101
+            Invoke-RestMethod "http://localhost:10001/api/notfound_b" | Should not be 101
         }
 
         Remove-Item $tempModule -Force
