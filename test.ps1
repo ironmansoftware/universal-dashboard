eb7fef6040d7156fdf09b244f79b7be301210bf9
Adam Driscoll <adam.r.driscoll@gmail.com>
2019-09-20 09:30:27 +0530

    Remove test script.

--- test.ps1
+++
@@ -1,7 +0,0 @@
-$Cert = New-SelfSignedCertificate -CertStoreLocation Cert:\CurrentUser\My -DnsName localhost
-
-Start-UDDashboard -Content {
-  New-UDDashboard -Title "Secure Dashboard" -Content {
-    New-UDElement -Tag "h1" -Content { "hi" }
-  }
-} -Port 12000 -Certificate $Cert
