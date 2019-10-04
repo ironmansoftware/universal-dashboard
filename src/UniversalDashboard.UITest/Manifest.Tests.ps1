f25dc9d48ee41fdf74734584bda3fbbf403d776b
Adam Driscoll <adamdriscoll@users.noreply.github.com>
2019-10-04 19:39:25 +0530

    Fix tool (#1224)
    * Add CLear-UDElement to manifest
    
    * Woops.
    
    * Fix nightly and manifest tests.

--- src/UniversalDashboard.UITest/Manifest.Tests.ps1
+++ src/UniversalDashboard.UITest/Manifest.Tests.ps1
@@ -119,8 +119,9 @@ Describe "Manifest" {
         Get-Command 'New-UDTabContainer' -ErrorAction SilentlyContinue | Should not be $null
         Get-Command 'New-UDTab' -ErrorAction SilentlyContinue | Should not be $null
         Get-Command 'New-UDGridLayout' -ErrorAction SilentlyContinue | Should not be $null
+        Get-Command 'Clear-UDCache' -ErrorAction SilentlyContinue | Should not be $null
         
-        (Get-Command -Module UniversalDashboard.Community | Measure-Object).Count | should be 124
+        (Get-Command -Module UniversalDashboard.Community | Measure-Object).Count | should be 125
     }
 
     It "should require .NET 4.7" -Skip  {
