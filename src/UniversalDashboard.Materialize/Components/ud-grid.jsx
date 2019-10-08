14b577294eb58e65583da7a12348a4e3239ea304
Adam Driscoll <adam.r.driscoll@gmail.com>
2019-10-08 09:40:11 -0600

    Fixes

--- src/UniversalDashboard.Materialize/Components/ud-grid.jsx
+++ src/UniversalDashboard.Materialize/Components/ud-grid.jsx
@@ -8,17 +8,34 @@ import {DebounceInput} from 'react-debounce-input';
 import { Dropdown, Button, Row, Col } from 'react-materialize';
 import UdIcon from './ud-icon.jsx';
 
+function strMapToObj(strMap) {
+    if (strMap == undefined) return null;
+    if (strMap._tail != undefined && strMap._tail.array != undefined) {
+
+        var items = [];
+        for(var i = 0; i < strMap.size; i++)
+        {
+            var item = strMap.get(i);
+            items.push(strMapToObj(item));
+        }
+
+        return items;
+    } 
+    if (!strMap.__iterate) return strMap;
+
+    let obj = Object.create({});
+    for (let [k,v] of strMap) {
+        // We don’t escape the key '__proto__'
+        // which can cause problems on older engines
+        obj[k] = strMapToObj(v);
+    }
+    return obj;
+}
+
 class CustomColumn extends React.Component {
     render() {
-
-        var value = JSON.parse(JSON.stringify([...this.props.value]))
-        var object = {};
-
-        value.forEach( item => {
-            object[item[0]] = item[1];
-        })
-
-        return <CustomCell value={object} dateTimeFormat={this.props.cellProperties.dateTimeFormat} />;
+        var value = strMapToObj(this.props.value);
+        return <CustomCell value={value} dateTimeFormat={this.props.cellProperties.dateTimeFormat} />;
     }
 }
 
