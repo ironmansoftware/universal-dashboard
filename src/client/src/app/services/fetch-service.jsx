77d95553c1193ff53b349fede29d6b65b5c84887
BoSen29 <54361737+BoSen29@users.noreply.github.com>
2019-11-12 13:33:32 +0100

    Fixes #1301
    Note: the firefox tests keep on failing on me, prolly due to verison. Check the devops tests.
    Seems like this function is only utilized by requeststate, and i've validated that this works wonderfully.

--- src/client/src/app/services/fetch-service.jsx
+++ src/client/src/app/services/fetch-service.jsx
@@ -46,11 +46,14 @@ export const fetchPost = function(url, data, success) {
         credentials: 'include'
       })
       .then(function(response){
-        if (response.status === 200) {
-            return response.json();
-        } else {
-            throw new Error(response.statusText);
-        }
+            var jsonresponse = response.json;
+
+            if (jsonresponse == null ) {
+                return true;
+            }
+            else {
+                return jsonresponse;
+            };
     })
     .then(success)
     .catch(function(e) {
