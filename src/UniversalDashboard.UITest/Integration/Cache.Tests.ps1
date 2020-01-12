0e1dc2ac08ef51b03743ab03a6876244c5a682c8
Adam Driscoll <adamdriscoll@users.noreply.github.com>
2020-01-12 12:15:34 -0700

    Fixing tests.

--- src/UniversalDashboard.UITest/Integration/Cache.Tests.ps1
+++ src/UniversalDashboard.UITest/Integration/Cache.Tests.ps1
@@ -23,30 +23,4 @@ Describe "Cache" {
             $Cache:data | Should be $null
         }
     }
-
-    Context "Xml in counter" {
-        $TempFile = [System.IO.Path]::GetTempFileName()
-        "<data><xpath></xpath><xpath></xpath><xpath></xpath></data>" | Out-File $TempFile
-        $cache:data = [xml](Get-Content $TempFile)
-
-        $dashboard = New-UDDashboard -Title "Test" -Content {
-            New-UDCounter -Id "Counter" -Endpoint {
-                ($Cache:data.SelectNodes("//data/xpath")).Count 
-            }
-        }
-
-        $Server = Start-UDDashboard -Port 10001 -Dashboard $dashboard 
-        $Driver = Start-SeFirefox
-        Enter-SeUrl -Driver $Driver -Url "http://localhost:$BrowserPort"
-
-        Start-Sleep 1
-
-        It "Should work with XML" {
-            (Find-SeElement -Id "Counter" -Driver $Driver).Text | Should be "3"
-        }
-
-        Stop-SeDriver $Driver
-        Stop-UDDashboard -Server $Server 
-        Remove-Item $TempFile
-    }
 }
