55f01a0b95f296d452bf2599fd562143cec71002
Adam Driscoll <adamdriscoll@users.noreply.github.com>
2020-02-20 16:52:32 -0700

    List tests.

--- src/UniversalDashboard.MaterialUI/dashboard.ps1
+++ src/UniversalDashboard.MaterialUI/dashboard.ps1
@@ -307,6 +307,69 @@ New-UDDashboard -Title "Dashboard" -Pages @(
         New-UDLink -text 'demo' -Id 'demo-link' -url '#' -variant body1 -ClassName 'gvili' -style @{color = 'red'}   
     }
 
+    New-UDPage -Name 'List' -Content {
+        $PaperStyle = @{ backgroundColor = 'darkslateblue'}
+        $ItemStyle = @{ backgroundColor = '#fff'; marginTop = '8px' } 
+        $NestedItemStyle = @{ backgroundColor = '#f8f8f8'}
+
+        New-UDPaper -Elevation 0 -Content {
+
+            New-UDList -Id 'listContent' -Content {
+
+                New-UDListItem -Id 'listContentItem' -AvatarType Avatar -Source 'https://pbs.twimg.com/profile_images/1065243723217416193/tg3XGcVR_400x400.jpg' -Label 'Adam Driscoll' -Content {
+
+                    New-UDListItem -Id 'list-item-security' -Label 'username and passwords' -Style $NestedItemStyle
+                    New-UDListItem -Id 'list-item-api' -Label 'api keys' -Style $NestedItemStyle
+
+                } -Style $ItemStyle
+
+                New-UDListItem -Id 'listContentItem' -Label 'Alon Gvili' -AvatarType Avatar -Source 'https://pbs.twimg.com/profile_images/847923651282423808/M-SFbPV1_400x400.jpg' -Content {
+
+                    New-UDListItem -Id 'list-item-inbox-new' -Label 'New Messages' -SubTitle 'You have 5 new messages.' -Style $NestedItemStyle
+                    New-UDListItem -Id 'list-item-inbox-unread' -Label 'Unread Messages' -SubTitle 'You have alot of unread messages.' -Style $NestedItemStyle
+
+                } -Style $ItemStyle
+
+                New-UDListItem -Id 'listContentItem' -Label "Alon Gvili" -AvatarType Avatar -Source 'https://pbs.twimg.com/profile_images/847923651282423808/M-SFbPV1_400x400.jpg' -IsButton -OnClick {
+                    Invoke-UDRedirect -Url 'https://github.com/AlonGvili' -OpenInNewWindow
+                } -Style $ItemStyle
+                New-UDListItem -Id 'listContentItem' -Label "Alon Gvili" -AvatarType Avatar -Source 'https://pbs.twimg.com/profile_images/847923651282423808/M-SFbPV1_400x400.jpg' -IsButton -OnClick {
+                    Invoke-UDRedirect -Url 'https://github.com/AlonGvili' -OpenInNewWindow
+                } -Style $ItemStyle
+                New-UDListItem -Id 'listContentItem' -Label "Alon Gvili" -AvatarType Avatar -Source 'https://pbs.twimg.com/profile_images/847923651282423808/M-SFbPV1_400x400.jpg' -IsButton -OnClick {
+                    Invoke-UDRedirect -Url 'https://github.com/AlonGvili' -OpenInNewWindow
+                } -Style $ItemStyle
+
+
+            } -SubHeader "USERS  $(0..50 | get-random)" -Style @{
+                backgroundColor = '#ececec'
+                paddingLeft = '8px'
+                paddingRight = '8px'
+                boxShadow = "0px 1px 5px 0px rgba(0,0,0,0.2),0px 2px 2px 0px rgba(0,0,0,0.14),0px 3px 1px -2px rgba(0,0,0,0.12)"
+            }           
+        } -IsEndPoint -AutoRefresh
+
+        $PaperStyle = @{ backgroundColor = 'darkslateblue'}
+        $ItemStyle = @{ backgroundColor = '#fff'; marginTop = '8px' } 
+        $NestedItemStyle = @{ backgroundColor = '#f8f8f8'}
+
+        New-UDList -Id 'demo-list' -Content {
+
+            New-UDListItem -Id 'list-item-user' -AvatarType Avatar -Source 'https://pbs.twimg.com/profile_images/1065243723217416193/tg3XGcVR_400x400.jpg' -Label 'Adam Driscoll' -Content {
+
+                New-UDListItem -Id 'list-item-security' -Label 'username and passwords' -Style $NestedItemStyle
+                New-UDListItem -Id 'list-item-api' -Label 'api keys' -Style $NestedItemStyle
+
+            } -Style $ItemStyle
+
+        } -SubHeader 'USERS' -Style @{
+            backgroundColor = '#ececec'
+            paddingLeft = '8px'
+            paddingRight = '8px'
+            boxShadow = "0px 1px 5px 0px rgba(0,0,0,0.2),0px 2px 2px 0px rgba(0,0,0,0.14),0px 3px 1px -2px rgba(0,0,0,0.12)"
+        }
+    }
+
     New-UDPage -Name "Paper" -Content {
         New-UDPaper -Content {
             New-UDHeading -Text "hi" -Id 'paperContent'
