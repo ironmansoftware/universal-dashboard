c9211ce2cbe9d4606c7bb96172fa95856096715e
Adam Driscoll <adamdriscoll@users.noreply.github.com>
2020-02-20 18:51:17 -0700

    Fix start dashboard tests.

--- src/UniversalDashboard.UITest/Cmdlet/Start-UDDashboard.Tests.ps1
+++ src/UniversalDashboard.UITest/Cmdlet/Start-UDDashboard.Tests.ps1
@@ -8,14 +8,14 @@ Get-UDDashboard | Stop-UDDashboard
 
 Describe "Start-UDDashboard" {
     It "starts the default dashboard" {
-        Start-UDDashboard -Port 10000 | Should not be $null
+        Start-UDDashboard -Port 10000 -Content {} | Should not be $null
     }
 
     Get-UDDashboard | Stop-UDDashboard
 
     It "starts a dashboard" {
 
-        $Dashboard = New-UDDashboard -Title "Test" -Content { New-UDCard -Title "Test"}
+        $Dashboard = New-UDDashboard -Title "Test" -Content { }
         
         Start-UDDashboard -Dashboard $Dashboard -Port 10000 | Should not be $null
     }
