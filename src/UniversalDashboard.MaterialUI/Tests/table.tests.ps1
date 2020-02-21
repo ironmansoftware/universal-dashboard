43b595e9f043a5b10c96383a5f3b23afc90fdf22
Adam Driscoll <adamdriscoll@users.noreply.github.com>
2020-02-21 12:38:44 -0700

    Fix table tests.

--- src/UniversalDashboard.MaterialUI/Tests/table.tests.ps1
+++ src/UniversalDashboard.MaterialUI/Tests/table.tests.ps1
@@ -2,6 +2,12 @@ Enter-SeUrl -Target $Driver -Url "http://localhost:10000/table"
 
 Describe "Table" {
     It 'has content' {
-        $Element = Find-SeElement -Id 'Tab1' -Driver $Driver | Invoke-SeClick 
+        $Element = Find-SeElement -Id 'defaultTable' -Driver $Driver 
+        ($Element.FindElementByTagName('tbody').FindElementsByTagName('tr') | Measure-Object).Count | should be 5
+    }
+
+    It 'has small size' {
+        $Element = Find-SeElement -Id 'smallTable' -Driver $Driver 
+        $Element.FindElementByTagName('thead').FindElementsByTagName('th')[0].GetAttribute('class').Contains('MuiTableCell-sizeSmall') | should be $true
     }
 }
