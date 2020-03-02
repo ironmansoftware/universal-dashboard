f17513d1064d98b5a9d13d50cffe31a741efc760
Adam Driscoll <adamdriscoll@users.noreply.github.com>
2020-03-02 09:10:38 -0700

    Add radios. (#1542)

--- src/UniversalDashboard.MaterialUI/Components/index.js
+++ src/UniversalDashboard.MaterialUI/Components/index.js
@@ -35,6 +35,7 @@ import UDNavbar from './framework/ud-navbar';
 import UDFooter from './framework/ud-footer';
 import UDAppBar from './appbar';
 import UDDrawer from './drawer';
+import {UDRadioGroup, UDRadio } from './radio';
 import NotFound from './framework/not-found';
 
 import {
@@ -79,6 +80,8 @@ UniversalDashboard.register('ud-navbar', UDNavbar);
 UniversalDashboard.register('ud-footer', UDFooter);
 UniversalDashboard.register('mu-appbar', UDAppBar);
 UniversalDashboard.register('mu-drawer', UDDrawer);
+UniversalDashboard.register('mu-radio', UDRadio);
+UniversalDashboard.register('mu-radiogroup', UDRadioGroup);
 
 // Framework Support
 import UdPage from './framework/ud-page';
