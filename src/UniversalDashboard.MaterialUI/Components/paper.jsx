e067f12efb3b518ce0b88543305888876bbbf8fd
Adam Driscoll <adamdriscoll@users.noreply.github.com>
2020-04-03 13:07:24 -0600

    Fix issue with paper.

--- src/UniversalDashboard.MaterialUI/Components/paper.jsx
+++ src/UniversalDashboard.MaterialUI/Components/paper.jsx
@@ -1,102 +1,34 @@
 /** @jsx jsx */
 import React from "react";
-import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
-import ReactInterval from "react-interval";
 import classNames from "classnames"
 import {jsx} from 'theme-ui'
-const styles = theme => ({
-  root: {
-    flexGrow: 1,
-    display: "flex",
-    padding: theme.spacing.unit * 2,
-    margin: theme.spacing.unit
-  }
-});
+import { withComponentFeatures } from './universal-dashboard';
 
-export class UdPaper extends React.Component {
-
-  state = {
-    children: this.props.children,
-  };
-
-  onLoadData = () => {
-    UniversalDashboard.get(`/api/internal/component/element/${this.props.id}`,(data) =>{
-      data.error ?
-      this.setState({
-        hasError: true,
-        error: data.error,
-        data: data,
-        errorMessage: data.error.message
-      }) :
-      this.setState({
-        children: data
-      })
-    })
-  }
-
-  componentWillMount = () => {
-    
-      this.onLoadData();
-    
-    this.pubSubToken = UniversalDashboard.subscribe(
-      this.props.id,
-      this.onIncomingEvent.bind(this)
-    );
-  };
-
-  onIncomingEvent(eventName, event) {
-    if (event.type === "syncElement") {
-      this.onLoadData();
-    }
-  }
-
-  UNSAFE_componentWillUnmount() {
-    UniversalDashboard.unsubscribe(this.pubSubToken);
-  }
-
-  render() {
-    const { 
-      classes,
-      elevation,
-      style,
-      height,
-      width,
-      square,
-      autoRefresh,
-      refreshInterval
-    } = this.props;
-    
-    const { children } = this.state;
-
-    return (
-      <>
-      <Paper 
-        id={this.props.id} 
-        className={classNames(classes.root, "ud-mu-paper")}
-        elevation={elevation}
-        style={{...style}}
-        height={height}
-        width={width}
-        square={square}  
-        sx={{ bg: 'primary', color: 'text'}}
-      >
-        
-        {UniversalDashboard.renderComponent(children)}
-
-      </Paper>
-      <ReactInterval
-      timeout={refreshInterval * 1000}
-      enabled={autoRefresh}
-      callback={this.onLoadData}/>
-    </>
-    );
-  }
+const UdPaper = (props) => {
+  const { 
+    classes,
+    elevation,
+    style,
+    height,
+    width,
+    square
+  } = props;
+  
+  return (
+    <Paper 
+      id={props.id} 
+      className={classNames("ud-mu-paper")}
+      elevation={elevation}
+      style={{...style}}
+      height={height}
+      width={width}
+      square={square}  
+      sx={{ bg: 'primary', color: 'text'}}
+    >
+      {props.render(props.children)}
+    </Paper>
+  );
 }
 
-UdPaper.propTypes = {
-  classes: PropTypes.object.isRequired
-};
-
-export default withStyles(styles)(UdPaper);
+export default withComponentFeatures(UdPaper);
